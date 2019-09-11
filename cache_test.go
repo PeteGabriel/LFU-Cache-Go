c07ccdbc602aa709c92fcf6a924da07dbf1fcb84
Pedro Gabriel <PeteGabriel@users.noreply.github.com>
2019-09-11 11:42:48 +0200

    Create cache_test.go

---
+++ cache_test.go
@@ -0,0 +1,31 @@
+package lfucgo
+
+import "testing"
+
+func TestNewCacheIsEmpty(t *testing.T){
+  cache:= new(Cache).Init()
+	if cache.GetElement(-1) != nil {
+		t.Error("Recent created cache must not contain any elements")
+	}
+}
+
+func TestCacheCanSaveNewElement(t *testing.T){
+  cache:= new(Cache).Init()
+	key:= 1
+	elem:= "NewElement"
+  if cache.Size() != 1 {
+		t.Error("Cache must insert new elements.")
+	}
+}
+
+func TestCacheCannotSaveElementWithExistentKey(t *testing.T){
+  cache:= new(Cache).Init()
+	key:= 1
+	elem:= "NewElement"
+	elem2:= "NewElement2"
+	cache.Insert(key, elem)
+	cache.Insert(key, elem2)
+	if cache.Size() != 1 {
+		t.Error("Cache cannot insert elements with same key.")
+	}
+}
