24a6f122c24182d33e1f5916b2da2526e2d7bce8
Pedro Gabriel <PeteGabriel@users.noreply.github.com>
2019-09-11 11:41:28 +0200

    Update cache.go

--- cache.go
+++ cache.go
@@ -1,18 +1,37 @@
 package lfucgo
 
-//Cache definition
 type Cache struct {
+  elems    map[int]interface{}
   freq *List
-	elem map[int]interface{}
 }
 
-//New cache
-func New() *Cache {
-	return new(Cache).init()
+// Init initializes or clears list l.
+func (c *Cache) Init() *Cache {
+  c.elems = make(map[int]interface{})
+  c.freq = new(List).Init()
+
+  c.freq.Push(&Pair{1, new(List).Init()})
+  return c
 }
 
-func (c *Cache) init() *Cache {
-  c.elem = make(map[int]interface{})
-	c.freq = new(List).Init()
-	return c
+
+//Get element by key without removing it.
+//This operation costs O(1).
+func (c *Cache) GetElement(key int) interface{} {
+  return nil
+}
+
+//Insert element by key
+//This operation costs O(1).
+func (c *Cache) Insert(key int, elem interface{}) bool {
+  c.elems[key] = elem
+  c.freq.Push
+  return true
+}
+
+//Remove element by key
+//This operation costs O(1).
+func (c *Cache) Remove(key int) {
+
+  delete(c.elements, key)
 }
